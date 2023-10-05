41e61827914f71b0a7050cdd6abc923c7f66e720
Messxrem <de.marusenko@gmail.com>
2023-10-05 18:25:43 +0300

    config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.10",
+  defaultNetwork: "hardhat",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:8545/"
+    },
+  }
 };
 
 export default config;
